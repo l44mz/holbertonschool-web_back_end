c36e33b3581cf8b1760a662450fc484deaa9a821
Lama <lamamqqz@gmail.com>
2026-08-25 15:53:46 +0300

    fix task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-"""Module that provides an async comprehension coroutine."""
-from typing import List
+''' Run time for four parallel comprehensions '''
+import asyncio
+import time
 
-async_generator = __import__('0-async_generator').async_generator
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def async_comprehension() -> List[float]:
-    """Collect 10 random numbers from async_generator using async for."""
-    return [i async for i in async_generator()]
+async def measure_runtime() -> float:
+    ''' Function to coutne '''
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end = time.perf_counter()
+    return end - start
